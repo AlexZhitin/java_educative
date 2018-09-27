8135c9232e86951373028c01f32666e6a8927bc9
SashaZ <aszhitin@gmail.com>
2018-09-27 18:15:48 +0300

    Создан пакет ua.stqa.educative.sandbox

--- sandbox/src/main/java/ua/stqa/educative/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ua/stqa/educative/sandbox/HelloWorld.java
@@ -1,4 +1,6 @@
-public class HelloWorld { 
+package ua.stqa.educative.sandbox;
+
+public class HelloWorld {
    public static void main(String[] args) { 
         System.out.println("Hello, World");
    }
