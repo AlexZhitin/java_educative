25abd65aa920cf583c9d4e7f39036af1703b64fe
SashaZ <aszhitin@gmail.com>
2018-09-27 18:56:35 +0300

    Проэкт отвязан от репозитория Git

--- sandbox/src/main/java/ua/stqa/educative/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ua/stqa/educative/sandbox/HelloWorld.java
@@ -1,7 +1,7 @@
 package ua.stqa.educative.sandbox;
 
 public class HelloWorld {
-   public static void main(String[] args) { 
+    public static void main(String[] args) {
         System.out.println("Hello, World");
-   }
+    }
 }
