546f72a2986bc83a6a61d864a715dcec09cead75
SashaZ <aszhitin@gmail.com>
2018-10-03 17:21:06 +0300

    Added a few functions

--- sandbox/src/main/java/ua/stqa/educative/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ua/stqa/educative/sandbox/HelloWorld.java
@@ -1,11 +1,30 @@
 package ua.stqa.educative.sandbox;
 
-public class HelloWorld {
-    public static void main(String[] args) {
-        hello();
-    }
+import java.sql.SQLOutput;
 
-    public static void hello() {
-        System.out.println ("Hello, world!");
-    }
+public class HelloWorld {
+  public static void main(String[] args) {
+    hello("world");
+    hello("guys");
+    hello("everybody");
+
+    double l = 5;
+    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
+
+    double a = 4;
+    double b = 3;
+    System.out.println("Площадь триугольника со сторонами " + a + " и " + " b = " + area(a, b));
+  }
+
+  public static void hello(String somebody) {
+    System.out.println("Hello " + somebody + "!");
+  }
+
+  public static double area(double len) {
+    return len * len;
+  }
+
+  public static double area(double a, double b) {
+    return a * b;
+  }
 }
