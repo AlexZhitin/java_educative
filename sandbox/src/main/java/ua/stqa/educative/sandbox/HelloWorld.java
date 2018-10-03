0e96bd323bc05ff52755a0c8a46ff82bfe7794bd
SashaZ <aszhitin@gmail.com>
2018-10-03 17:49:51 +0300

    Reverted version

--- sandbox/src/main/java/ua/stqa/educative/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ua/stqa/educative/sandbox/HelloWorld.java
@@ -2,6 +2,6 @@ package ua.stqa.educative.sandbox;
 
 public class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello, World");
+        System.out.println("Hello, Everybody");
     }
 }
