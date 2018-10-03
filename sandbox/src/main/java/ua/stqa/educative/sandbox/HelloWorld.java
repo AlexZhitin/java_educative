0b3f81020315ba6ad5eb366eab5060bba2ec2bc4
SashaZ <aszhitin@gmail.com>
2018-10-03 17:48:46 +0300

    Revert "Создана функция"
    This reverts commit caa038398643de4d4b1f15936efeb4bc56372e8a.
    
    # Conflicts:
    #	.idea/workspace.xml
    #	sandbox/out/production/classes/ua/stqa/educative/sandbox/HelloWorld.class
    #	sandbox/src/main/java/ua/stqa/educative/sandbox/HelloWorld.java

--- sandbox/src/main/java/ua/stqa/educative/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ua/stqa/educative/sandbox/HelloWorld.java
@@ -1,42 +1,7 @@
 package ua.stqa.educative.sandbox;
 
-import java.sql.SQLOutput;
-
 public class HelloWorld {
-<<<<<<< HEAD
-<<<<<<< HEAD
-<<<<<<< HEAD
-  public static void main(String[] args) {
-    hello("world");
-    hello("guys");
-    hello("everybody");
-
-    double l = 5;
-    System.out.println("Площадь квадрата со стороной " + l + " = " + area(l));
-
-    double a = 4;
-    double b = 3;
-    System.out.println("Площадь триугольника со сторонами " + a + " и " + " b = " + area(a, b));
-  }
-
-  public static void hello(String somebody) {
-    System.out.println("Hello " + somebody + "!");
-  }
-
-  public static double area(double len) {
-    return len * len;
-  }
-
-  public static double area(double a, double b) {
-    return a * b;
-  }
-=======
-=======
->>>>>>> parent of caa0383... Создана функция
-=======
->>>>>>> parent of caa0383... Создана функция
     public static void main(String[] args) {
         System.out.println("Hello, World");
     }
->>>>>>> parent of caa0383... Создана функция
 }
