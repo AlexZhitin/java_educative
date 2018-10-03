63d773d9fa499750848a4afff62b5cf94743366f
SashaZ <aszhitin@gmail.com>
2018-10-03 18:54:02 +0300

    Добавлено несколько функций

--- sandbox/src/main/java/ua/stqa/educative/sandbox/HelloWorld.java
+++ sandbox/src/main/java/ua/stqa/educative/sandbox/HelloWorld.java
@@ -2,11 +2,18 @@ package ua.stqa.educative.sandbox;
 
 public class HelloWorld {
 
-  public static void main (String[] args) {
-    hello ("world");
+  public static void main(String[] args) {
+    hello("world");
+
+    double len = 6;
+    System.out.println("Площадь квадрата со стороной " + len + " = " + area(len));
   }
 
-  public static void hello (String somebody) {
+  public static void hello(String somebody) {
     System.out.println("Hello, " + somebody + "!");
+  }
+
+  public static double area(double l) {
+    return l * l;
   }
 }
